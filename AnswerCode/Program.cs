8f89f978d8e5c821e5cb41c60d700fb66bdc30ee
smc9759 <42694805+smc9759@users.noreply.github.com>
2023-09-21 15:54:32 +0900

    Update Program.cs

--- AnswerCode/Program.cs
+++ AnswerCode/Program.cs
@@ -8,8 +8,18 @@ namespace AnswerCode
 {
     class Program
     {
-        static void Main(string[] args)
+        static int Main(string[] args)
         {
+            string szInput1 =  Console.ReadLine();
+            string szInput2 =  Console.ReadLine();
+            int nInput1 = int.Parse(szInput1);
+
+            int nInput2 = int.Parse(szInput2);
+            int nResult = nInput1 + nInput2;
+            Console.WriteLine("{0}", nResult);
+
+
+            return 0;
         }
     }
 }
