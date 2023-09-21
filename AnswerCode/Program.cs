a201ec209fdd09e065e91df282bef7fb1963613d
smc9759 <42694805+smc9759@users.noreply.github.com>
2023-09-21 17:55:41 +0900

    Update Program.cs

--- AnswerCode/Program.cs
+++ AnswerCode/Program.cs
@@ -10,28 +10,12 @@ namespace AnswerCode
     {
         static int Main(string[] args)
         {
-            string szInput1 = Console.ReadLine();
-            string szInput2 = Console.ReadLine();
-
-            int nInput1 = int.Parse(szInput1);
-
-            int nInput2Firstletter = szInput2[0] - '0';
-            int nInput2Secondletter = szInput2[1] - '0';
-            int nInput2Thirdletter = szInput2[2] - '0';
-
-            int line3, line4, line5 = 0;
-
-            line3 = nInput1 * nInput2Thirdletter;
-            line4 = nInput1 * nInput2Secondletter;
-            line5 = nInput1 * nInput2Firstletter;
-
-            int result = line3 + line4 * 10 + line5 * 100;
-
-            Console.WriteLine(line3);
-            Console.WriteLine(line4);
-            Console.WriteLine(line5);
-            Console.WriteLine(result);
-
+            string[] szInput1 = Console.ReadLine().Split();
+            long A = long.Parse(szInput1[0]);
+            long B = long.Parse(szInput1[1]);
+            long C = long.Parse(szInput1[2]);
+            Console.WriteLine(A+B+C);
+            
             return 0;
         }
     }
